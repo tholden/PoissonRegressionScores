fbeecf995b3b337cbf29a5df22b1a857c9950d49
Tom Holden <thomas.holden@gmail.com>
2023-12-20 20:24:25 +0100

    Introduce time varying paramters

--- EstimatePoissionRegressionOnScores.m
+++ EstimatePoissionRegressionOnScores.m
@@ -13,29 +13,79 @@ function [ Mean, HomeOffset, OvertimeOffset, Offense, Defense, Teams ] = Estimat
     OffenseIDs = reshape( OffenseIDs, NMatches, 2 );
     DefenseIDs = fliplr( OffenseIDs );
 
-    Home = [ zeros( NMatches, 1 ); double( ~NeutralSite ) ];
+    Home = tvec( [ zeros( NMatches, 1 ), double( ~NeutralSite ) ] );
 
-    Scores = Scores(:);
-    Overtime = double( [ Overtime(:); Overtime(:); ] );
-    OffenseIDs = OffenseIDs(:);
-    DefenseIDs = DefenseIDs(:);
+    Scores = tvec( Scores );
+    Overtime = tvec( double( [ Overtime(:), Overtime(:) ] ) );
+    OffenseIDs = tvec( OffenseIDs );
+    DefenseIDs = tvec( DefenseIDs );
 
     NObservations = 2 * NMatches;
 
-    OffenseIndicators = sparse( ( 1 : NObservations ).', OffenseIDs, ones( NObservations, 1 ), NObservations, NTeams, NObservations );
-    DefenseIndicators = sparse( ( 1 : NObservations ).', DefenseIDs, ones( NObservations, 1 ), NObservations, NTeams, NObservations );
+    Weeks = zeros( NObservations, 1 );
+
+    PlayedThisWeek = zeros( 1, NTeams );
+
+    NWeeks = 1;
+
+    for t = 1 : NObservations
+
+        PlayedThisWeek( OffenseIDs( t ) ) = PlayedThisWeek( OffenseIDs( t ) ) + 1;
+        PlayedThisWeek( DefenseIDs( t ) ) = PlayedThisWeek( DefenseIDs( t ) ) + 1;
+
+        if any( PlayedThisWeek > 2 )
+
+            PlayedThisWeek = zeros( 1, NTeams );
+            NWeeks = NWeeks + 1;
+
+        end
+
+        Weeks( t ) = NWeeks;
+
+    end
+
+    yalmip clear;
 
     Mean = sdpvar( 1, 1 );
     HomeOffset = sdpvar( 1, 1 );
     OvertimeOffset = sdpvar( 1, 1 );
-    Offense = sdpvar( NTeams, 1 );
-    Defense = sdpvar( NTeams, 1 );
 
-    LogPrediction = Mean + Home * HomeOffset + Overtime * OvertimeOffset + OffenseIndicators * Offense - DefenseIndicators * Defense;
+    OffenseIntercept = sdpvar( 1, NTeams );
+    DefenseIntercept = sdpvar( 1, NTeams );
+    OffenseSlope = sdpvar( 1, NTeams );
+    DefenseSlope = sdpvar( 1, NTeams );
 
-    LogLikelihood = sum( Scores .* LogPrediction - exp( LogPrediction ) );
+    Offense = bsxfun( @plus, OffenseIntercept, ( 1 : NWeeks ).' * OffenseSlope );
+    Defense = bsxfun( @plus, DefenseIntercept, ( 1 : NWeeks ).' * DefenseSlope );
 
-    Conditions = [ sum( Offense ) == 0, sum( Defense ) == 0 ];
+    LogPrediction = Mean + Home * HomeOffset + Overtime * OvertimeOffset + Offense( sub2ind( [ NWeeks, NTeams ], Weeks, OffenseIDs ) ) - Defense( sub2ind( [ NWeeks, NTeams ], Weeks, DefenseIDs ) );
+
+    LogLikelihoods = Scores .* LogPrediction - exp( LogPrediction );
+
+    LogLikelihood = sum( LogLikelihoods );
+
+    Conditions = [ sum( OffenseIntercept ) == 0, sum( DefenseIntercept ) == 0, sum( OffenseSlope ) == 0, sum( DefenseSlope ) == 0 ];
+
+    SolverOutput = optimize( Conditions, -LogLikelihood, sdpsettings( 'verbose', 1, 'showprogress', 1, 'warning', 1, 'cachesolvers', 1, 'solver', 'mosek' ) );
+
+    assert( SolverOutput.problem == 0 );
+
+    MaxOffenseVariance = value( mean( diff( Offense ) .^ 2 ) );
+    MaxDefenseVariance = value( mean( diff( Defense ) .^ 2 ) );
+
+    Offense = sdpvar( NWeeks, NTeams, 'full' );
+    Defense = sdpvar( NWeeks, NTeams, 'full' );
+
+    OffenseVariance = mean( diff( Offense ) .^ 2 );
+    DefenseVariance = mean( diff( Defense ) .^ 2 );
+
+    LogPrediction = Mean + Home * HomeOffset + Overtime * OvertimeOffset + Offense( sub2ind( [ NWeeks, NTeams ], Weeks, OffenseIDs ) ) - Defense( sub2ind( [ NWeeks, NTeams ], Weeks, DefenseIDs ) );
+
+    LogLikelihoods = Scores .* LogPrediction - exp( LogPrediction );
+
+    LogLikelihood = sum( LogLikelihoods );
+
+    Conditions = [ sum( Offense(:) ) == 0, sum( Defense(:) ) == 0, OffenseVariance <= MaxOffenseVariance, DefenseVariance <= MaxDefenseVariance ];
 
     SolverOutput = optimize( Conditions, -LogLikelihood, sdpsettings( 'verbose', 1, 'showprogress', 1, 'warning', 1, 'cachesolvers', 1, 'solver', 'mosek' ) );
 
@@ -44,7 +94,14 @@ function [ Mean, HomeOffset, OvertimeOffset, Offense, Defense, Teams ] = Estimat
     Mean = value( Mean );
     HomeOffset = value( HomeOffset );
     OvertimeOffset = value( OvertimeOffset );
-    Offense = value( Offense );
-    Defense = value( Defense );
+    Offense = value( Offense( end, : ).' );
+    Defense = value( Defense( end, : ).' );
+
+end
+
+function z = tvec( z )
+
+    z = z.';
+    z = z(:);
 
 end
