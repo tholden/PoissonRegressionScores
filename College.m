1fed7c17cc5ade3e13b8e0cda292b450c2f100c2
Tom Holden <thomas.holden@gmail.com>
2024-01-02 15:43:59 +0100

    update college to use postseason results too

--- College.m
+++ College.m
@@ -2,14 +2,15 @@ clear variables;
 
 addpath( genpath( 'YALMIP' ) );
 
-% College.csv is downloaded from https://collegefootballdata.com/exporter/games?year=2023&seasonType=regular&division=fbs
+% College.csv is downloaded from https://collegefootballdata.com/exporter/games?year=2023&seasonType=both&division=fbs
 
 warning( 'off', 'MATLAB:table:ModifiedAndSavedVarnames' );
 
 Table = readtable( 'College.csv', 'TextType', 'string' );
 
+Table = sortrows( Table, { 'StartDate', 'Id' } );
+
 Table( Table.Season ~= 2023, : ) = [];
-Table( Table.SeasonType ~= "regular", : ) = [];
 Table( Table.HomeDivision ~= "fbs", : ) = [];
 Table( Table.AwayDivision ~= "fbs", : ) = [];
 
@@ -19,7 +20,7 @@ Table( ~isfinite( Table.AwayPoints ), : ) = [];
 
 Scores = [ Table.AwayPoints, Table.HomePoints ];
 IDs = [ Table.AwayTeam, Table.HomeTeam ];
-Overtime = false( size( Scores, 1 ), 1 );
+Overtime = ( Table.AwayPoints > Table.AwayLineScores_0_ + Table.AwayLineScores_1_ + Table.AwayLineScores_2_ + Table.AwayLineScores_3_ ) | ( Table.HomePoints > Table.HomeLineScores_0_ + Table.HomeLineScores_1_ + Table.HomeLineScores_2_ + Table.HomeLineScores_3_ );
 NeutralSite = Table.NeutralSite == "true";
 
 [ Mean, HomeOffset, OvertimeOffset, Offense, Defense, Teams ] = EstimatePoissionRegressionOnScores( Scores, IDs, Overtime, NeutralSite, 1 );
